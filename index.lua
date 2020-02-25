c4de8910ffe6bb69cecc48212d05bd002fd59590
GitHub Action <action@github.com>
2020-02-25 10:10:27 +0000

    ci: Automated build

--- index.lua
+++ index.lua
@@ -14,6 +14,11 @@ local lobby = [[<C><P /><Z><S><S X="401" Y="396" T="0" L="804" H="20" P="0,0,0.3
 
 --game variables
 local points, players, lands = {}, {}, {}
+local gameStarted = false
+local totalPlayers = 0
+
+--Timers4TFm
+local a={}a.__index=a;a._timers={}a._init=false;a._clock=0;setmetatable(a,{__call=function(b,...)return b.new(...)end})function a.init(c)if not a._init then a._init=true;a._clock=c end end;function a.process(d)a._clock=d;for e,f in next,a._timers do if f:isAlive()and f:getMatureTime()<=a._clock then f:call()if f.loop then f:reset()else f:kill()end end end end;function a.run(d)a.init(d)a.process(d)end;function a.new(g,h,i,j,...)local self=setmetatable({},a)self.id=g;self.callback=h;self.timeout=i;self.mature=a._clock+i;self.loop=j;self.args={...}self.alive=true;a._timers[g]=self;return self end;function a:getId()return self.id end;function a:getTimeout()return self.timeout end;function a:isLooping()return self.loop end;function a:getMatureTime()return self.mature end;function a:isAlive()return self.alive end;function a:setCallback(k)self.callback=k end;function a:addTime(c)self.mature=self.mature+c end;function a:setLoop(j)self.loop=j end;function a:setArgs(...)self.args={...}end;function a:call()self.callback(table.unpack(self.args))end;function a:kill()self.alive=false;self=nil end;function a:reset()self.mature=a._clock+self.timeout end;Timer=a
 
 --[[ Player class ]]--
 local Player = {}
@@ -109,7 +114,8 @@ end
 tfm.exec.disableAutoNewGame()
 tfm.exec.disableAutoTimeLeft()
 tfm.exec.disableAfkDeath()
-
+tfm.exec.disableAutoShaman()
+tfm.exec.disableMortCommand()
 
 function initLands()
 
@@ -171,7 +177,7 @@ function displayLands(target)
 end
 
 function main()
-    tfm.exec.newGame(map)
+    tfm.exec.newGame(lobby)
 end
 
 
@@ -179,27 +185,56 @@ end
 
 function eventNewGame()
     
-    --parsing the map's xml and getting the land points
-    local mapDom = parseXml(map, true)
-    for _, o in next, path(mapDom, "Z", "S", "S") do
-        if o.attribute.lua then
-            points[tonumber(o.attribute.lua)] = {x = o.attribute.X, y = o.attribute.Y}
+    if gameStarted then
+        --parsing the map's xml and getting the land points
+        local mapDom = parseXml(map, true)
+        for _, o in next, path(mapDom, "Z", "S", "S") do
+            if o.attribute.lua then
+                points[tonumber(o.attribute.lua)] = {x = o.attribute.X, y = o.attribute.Y}
+            end
+        end
+
+        --initializing the players
+        for name, player in next, tfm.get.room.playerList do
+            players[name] = Player(name) 
+        end
+
+        --initializing the lands
+        initLands()
+   
+    else
+        for _, _ in next, tfm.get.room.playerList do
+            totalPlayers = totalPlayers + 1
+        end
+        if totalPlayers >= 2 then
+            tfm.exec.chatMessage("Starting the game in 10 seconds...")
+            Timer("start game", function()
+                gameStarted = true
+                tfm.exec.newGame(map)
+            end, 10000, false)
         end
     end
-
-    --initializing the players
-    for name, player in next, tfm.get.room.playerList do
-        players[name] = Player(name) 
-    end
-
-    --initializing the lands
-    initLands()
 
 end
 
 function eventNewPlayer(name)
-    displayLands(name)
+    totalPlayers = totalPlayers + 1
+    if gameStarted then
+        displayLands(name)
+    elseif not gameStarted and totalPlayers >= 2 then
+        tfm.exec.chatMessage("Starting the game in 10 seconds...")
+        Timer("start game", function()
+            gameStarted = true
+            tfm.exec.newGame(map)
+        end, 10000, false)
+    end
 end
 
+function eventPlayerLeft(name)
+    totalPlayers = totalPlayers - 1
+end
+function eventLoop(tc, tr)
+    Timer.run(tc)
+end
 
 main()
