e6c193542396f1c92a0430ee2e532279e7db3c0d
GitHub Action <action@github.com>
2020-02-25 07:44:50 +0000

    ci: Automated build

--- index.lua
+++ index.lua
@@ -4,27 +4,12 @@ local a="Makinit's XML library"local b="[%a_:][%w%.%-_:]*"function parseXml(c,d)
 
 --==[[ map ]]==--
 
-local map = [[<C><P /><Z><S><S X="398" L="806" Y="417" H="30" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="400" L="806" Y="1" H="30" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="0" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="600" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="60" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="360" L="16" Y="212" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="420" L="16" Y="212" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="480" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="540" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="40" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="298" L="602" Y="200" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="240" L="16" Y="210" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="120" L="16" Y="210" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="180" L="16" Y="208" H="400" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="80" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="160" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="120" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="244" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="284" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="300" L="602" Y="324" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="298" L="602" Y="364" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="299" L="488" Y="205" H="334" P="0,0,0.3,0.2,0,0,0,0" T="16" /><S X="571" m="" lua="1" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="573" m="" lua="28" L="10" Y="25" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="332" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="5" /><S X="331" m="" lua="24" L="10" Y="24" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="450" m="" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="3" /><S X="452" m="" lua="26" L="10" Y="25" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="511" m="" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="2" /><S X="513" m="" lua="27" L="10" Y="25" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="270" m="" L="10" Y="389" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="6" /><S X="271" m="" lua="23" L="10" Y="26" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="389" m="" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="4" /><S X="391" m="" lua="25" L="10" Y="24" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="208" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="7" /><S X="210" m="" lua="22" L="10" Y="26" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="90" m="" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="9" /><S X="92" m="" L="10" Y="24" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="20" /><S X="151" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="8" /><S X="150" m="" lua="21" L="10" Y="25" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="30" m="" lua="10" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="19" L="10" Y="24" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="18" L="10" Y="58" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="29" L="10" Y="58" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="14" L="10" Y="220" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="33" L="10" Y="220" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="17" L="10" Y="97" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="30" L="10" Y="97" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="13" L="10" Y="259" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="34" L="10" Y="259" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="16" L="10" Y="137" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="31" L="10" Y="137" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="12" L="10" Y="299" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="35" L="10" Y="299" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="15" L="10" Y="176" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="32" L="10" Y="176" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="32" m="" lua="11" L="10" Y="338" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="571" m="" lua="36" L="10" Y="338" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /></S><D><DS X="757" Y="380" /></D><O /><L /></Z></C>
-]]
-
---==[[ main ]]==--
-
---disabling unwanted room settings
-tfm.exec.disableAutoNewGame()
-tfm.exec.disableAutoTimeLeft()
-tfm.exec.disableAfkDeath()
-
---game variables
-local points, players
-
-function main()
-    points = {}
-    players = {}
-    tfm.exec.newGame(map)
-end
-
+local map = [[<C><P /><Z><S><S X="398" L="806" Y="417" H="30" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="397" L="806" Y="-73" H="30" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="67" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="667" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="127" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="426" L="16" Y="212" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="486" L="16" Y="212" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="547" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="607" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="336" L="665" Y="40" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="338" L="657" Y="200" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="307" L="16" Y="210" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="367" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="187" L="16" Y="210" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="247" L="16" Y="208" H="490" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="338" L="661" Y="81" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="335" L="667" Y="160" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="336" L="665" Y="120" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="340" L="657" Y="244" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="337" L="662" Y="284" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="336" L="664" Y="324" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="337" L="659" Y="364" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="341" L="538" Y="189" H="367" P="0,0,0.3,0.2,0,0,0,0" T="16" /><S X="638" m="" lua="1" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="458" m="" lua="28" L="10" Y="-20" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="399" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="5" /><S X="216" m="" lua="24" L="10" Y="-21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="517" m="" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="3" /><S X="337" m="" lua="26" L="10" Y="-20" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="578" m="" L="10" Y="387" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="2" /><S X="398" m="" lua="27" L="10" Y="-20" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="337" m="" L="10" Y="389" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="6" /><S X="156" m="" lua="23" L="10" Y="-19" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="456" m="" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="4" /><S X="276" m="" lua="25" L="10" Y="-21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="275" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="7" /><S X="95" m="" lua="22" L="10" Y="-19" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="157" m="" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="9" /><S X="37" m="" L="10" Y="21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="20" /><S X="218" m="" L="10" Y="388" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" lua="8" /><S X="36" m="" lua="21" L="10" Y="-21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="97" m="" lua="10" L="10" Y="386" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="38" m="" lua="19" L="10" Y="58" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="39" m="" lua="18" L="10" Y="98" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="516" m="" lua="29" L="10" Y="-22" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="14" L="10" Y="267" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="33" L="10" Y="62" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="17" L="10" Y="144" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="578" m="" lua="30" L="10" Y="-21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="13" L="10" Y="306" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="34" L="10" Y="101" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="16" L="10" Y="184" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="31" L="10" Y="-21" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="36" m="" lua="12" L="10" Y="346" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="35" L="10" Y="141" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="15" L="10" Y="223" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="32" L="10" Y="18" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="37" m="" lua="11" L="10" Y="385" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="36" L="10" Y="180" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="5" L="17" Y="182" H="452" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="336" L="665" Y="1" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="336" L="665" Y="-42" H="16" P="0,0,0.3,0.2,0,0,0,0" T="0" /><S X="638" m="" lua="39" L="10" Y="306" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="40" L="10" Y="344" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="37" L="10" Y="222" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /><S X="638" m="" lua="38" L="10" Y="261" H="10" P="0,0,0.3,0.2,0,0,0,0" T="8" /></S><D><DS X="757" Y="380" /></D><O /><L /></Z></C>]]
 
 --==[[ libs ]]==--
+
+--game variables
+local points, players, lands = {}, {}, {}
 
 --[[ Player class ]]--
 local Player = {}
@@ -51,7 +36,131 @@ function Player:goTo(land)
     tfm.exec.movePlayer(self.name, points[land].x, points[land].y, false)
 end
 
---Not implemented
+--[[ Land class ]]
+local Land = {}
+Land.__index = Land
+Land.__tostring = function(self)
+    return "[name=" .. self.name .. "]"
+end
+
+setmetatable(Land, {
+    __call = function (cls, name, ...)
+        return cls.new(name, ...)
+    end,
+})
+
+function Land.new(name, price, color, landIndex, isSpecial)
+    local self = setmetatable({}, Land)
+    self.name = name
+    self.price = price
+    self.color = color
+    self.landIndex = landIndex
+    self.isSpecial = isSpecial
+    self.houses = 0
+    self.hasHotel = false
+    self.owner = nil
+    self.onland = nil
+    self.locX = points[landIndex].x
+    self.locY = points[landIndex].y
+    return self
+end
+
+function Land:removeOwner(owner)
+    self.owner = nil
+end
+
+function Land:setOwner(owner)
+    if owner then
+        error(self.name .. " already has an owner")
+    end
+    self.owner = owner
+end
+
+function Land:addHouse()
+    if self.houses >= 4 or self.hasHotel then
+        error("Can't build a house here")
+    else
+        self.houses = self.houses + 1
+    end
+end
+
+function Land:removeHouse()
+    self.houses = (self.houses - 1 < 0) and 0 or self.houses - 1
+end
+
+function Land:addHotel()
+    self.houses = 0
+    self.hasHotel = true
+end
+
+--[[@abstract method]]--
+function Land:onLand(player, ...) 
+    return false
+end
+
+
+--==[[ main ]]==--
+
+--disabling unwanted room settings
+tfm.exec.disableAutoNewGame()
+tfm.exec.disableAutoTimeLeft()
+tfm.exec.disableAfkDeath()
+
+function initLands()
+
+    -- configuring the lands
+    lands[1] = Land("Go", nil, nil, 1, true)
+    lands[2] = Land("Buffy`s Residence", 600, "purple", 2)
+    lands[3] = Land("Community Chest", nil, nil, 3, true)
+    lands[4] = Land("Transformice Museum", 600, "purple", 4)
+    lands[5] = Land("Income Tax", nil, nil, 5, true)
+    lands[6] = Land("Shaman`s Teleporter", 2000, "teleporter", 6)
+    lands[7] = Land("801 Town", 1000, "light blue", 7)
+    lands[8] = Land("Chance", nil, nil, 8, true)
+    lands[9] = Land("Grim`s Mansion", 1000, "light blue", 9)
+    lands[10] = Land("Nuremberg", 1200, "light blue", 10)
+    lands[11] = Land("Jail", nil, nil, 11, true)
+    lands[12] = Land("Jingle`s abode", 1400, "magenta", 12)
+    lands[13] = Land("Cheese factory", 1500, "factory", 13)
+    lands[14] = Land("Salvador", 1400, "magenta", 14)
+    lands[15] = Land("Charlotte`s Tree Stump", 1600, "magenta", 15)
+    lands[16] = Land("Cheese Hole Teleporter", 2000, "teleporter", 16)
+    lands[17] = Land("King Fromagnus`s Castle", 1800, "orange", 17)
+    lands[18] = Land("Community Chest", nil, nil, 18, true)
+    lands[19] = Land("Land of Dream", 1800, "orange", 19)
+    lands[20] = Land("Garden of Roses", 2000, "orange", 20)
+    lands[21] = Land("Free Parking", nil, nil, 21, true)
+    lands[22] = Land("Ol` Souris", 2200, "red", 22)
+    lands[23] = Land("Chance", nil, nil, 23, true)
+    lands[24] = Land("Cheze Gustave", 2200, "red", 24)
+    lands[25] = Land("Konomouse", 2400, "red", 25)
+    lands[26] = Land("Elisah`s Teleporter", 2000, "teleporter", 26)
+    lands[27] = Land("Kürbisses-Nacht Streets", 2600, "yellow", 27)
+    lands[28] = Land("Kürbisses-Nacht Attic", 2600, "yellow", 28)
+    lands[29] = Land("Fraises Factory", 1500, "factory", 29)
+    lands[30] = Land("Kürbisses-Nacht Graveyard", 2800, "yellow", 30)
+    lands[31] = Land("Go to Jail", nil, nil, 31, true)
+    lands[32] = Land("Amazonia", 3000, "green", 32)
+    lands[33] = Land("Antic Jungle", 3000, "green", 33)
+    lands[34] = Land("Community Chest", nil, nil, 34, true)
+    lands[35] = Land("Crystal Jungle", 3000, "green", 35)
+    lands[36] = Land("Portal Teleporter", 2000, "teleporter", 36)
+    lands[37] = Land("Chance", nil, nil, 37, true)
+    lands[38] = Land("Haunted Library", 3500, "dark blue", 38)
+    lands[39] = Land("Super Tax", nil, nil, 39, true)
+    lands[40] = Land("Temple", 4000, "dark blue", 40)
+
+    --overriding the behaviours of special lands
+    lands[1].onLand = function(self, player)
+        player.money = player.money + 2000
+    end
+
+end
+
+function main()
+    tfm.exec.newGame(map)
+end
+
 
 --==[[ events ]]==--
 
@@ -69,6 +178,9 @@ function eventNewGame()
     for name, player in next, tfm.get.room.playerList do
         players[name] = Player(name) 
     end
+
+    --initializing the lands
+    initLands()
 
 end
 
