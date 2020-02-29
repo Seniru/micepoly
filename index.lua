be34406edf335e11ee74c75fee13245a5f3e601d
GitHub Action <action@github.com>
2020-02-29 09:43:18 +0000

    ci: Automated build

--- index.lua
+++ index.lua
@@ -21,6 +21,55 @@ local current = nil
 --Timers4TFm
 local a={}a.__index=a;a._timers={}a._init=false;a._clock=0;setmetatable(a,{__call=function(b,...)return b.new(...)end})function a.init(c)if not a._init then a._init=true;a._clock=c end end;function a.process(d)a._clock=d;for e,f in next,a._timers do if f:isAlive()and f:getMatureTime()<=a._clock then f:call()if f.loop then f:reset()else f:kill()end end end end;function a.run(d)a.init(d)a.process(d)end;function a.new(g,h,i,j,...)local self=setmetatable({},a)self.id=g;self.callback=h;self.timeout=i;self.mature=a._clock+i;self.loop=j;self.args={...}self.alive=true;a._timers[g]=self;return self end;function a:getId()return self.id end;function a:getTimeout()return self.timeout end;function a:isLooping()return self.loop end;function a:getMatureTime()return self.mature end;function a:isAlive()return self.alive end;function a:setCallback(k)self.callback=k end;function a:addTime(c)self.mature=self.mature+c end;function a:setLoop(j)self.loop=j end;function a:setArgs(...)self.args={...}end;function a:call()self.callback(table.unpack(self.args))end;function a:kill()self.alive=false;self=nil end;function a:reset()self.mature=a._clock+self.timeout end;Timer=a
 
+--[[ Card class ]]--
+local Card = {id=0, header="", description=""}
+
+Card.__index = Card
+Card.__tostring = function(self)
+    return self.description
+end
+
+setmetatable(Card, {
+    __call = function (cls, name)
+        return cls:new(name)
+    end,
+})
+
+
+function Card:new(id, header, desc)
+    local self = setmetatable({}, self)
+    self.id = id or 0
+    self.header = header or ""
+    self.description = desc or ""
+    return self
+end
+
+--[[
+    @abstract method
+]]
+function Card:action(player, ...)
+    error("Calling non-overidden abstract methods")
+end
+
+--[[ Chance class ]]--
+local Chance = Card:new()
+Chance.__index = Card
+Chance.__tostring = function(self)
+    return "[name=" .. self.name .. ", money: " .. self.money .. "]"
+end
+
+setmetatable(Chance, {
+    __call = function (cls, name)
+        return cls.new(name)
+    end,
+})
+
+function Chance:new(id, header, desc)
+    --class Chance extends Card
+    local self = setmetatable(Card:new(id, header, desc), self)
+    return self
+end
+
 --[[ Player class ]]--
 local Player = {}
 Player.__index = Player
